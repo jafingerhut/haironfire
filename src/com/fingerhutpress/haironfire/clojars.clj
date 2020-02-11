3a8721a35050a70b518891314b8f46f5091fdb3d
Andy Fingerhut <andy_fingerhut@alum.wustl.edu>
2020-02-10 21:40:46 -0800

    Add functions for determining 'canonical' artifacts, and ... writing bash script that does 'git clone' once for each distinct Github URL found.

--- src/com/fingerhutpress/haironfire/clojars.clj
+++ src/com/fingerhutpress/haironfire/clojars.clj
@@ -124,9 +124,75 @@
       ;; else no :url key
       {:artifact-loc-type :no-scm-and-no-url})))
 
+;; I have found that as of 2020-Feb on Clojars feed.clj.gz file about
+;; artifacts, none of them contain the ":" character in the group-id
+;; string, nor in the artifact-id string.  It is a legal character in
+;; a Unix/Linux file name, so I will use it as a separator between
+;; them when creating some file names.
+
+(def group-id-artifact-id-separator ":")
+
 (defn group-artifact-id
   [art-map]
-  (str (:group-id art-map) ":" (:artifact-id art-map)))
+  (str (:group-id art-map)
+       group-id-artifact-id-separator
+       (:artifact-id art-map)))
+
+;; I have found that there are many groups of artifacts listed on
+;; Clojars.org in the feed.clj.gz file that have the same Github.com
+;; URL for source code.  When retrieving source code, I would like to
+;; get at most one copy of each Github repository, to save download
+;; time, but also disk space, as there are some not-small-disk-space
+;; projects that are represented over 10 times each in feed.clj.gz,
+;; probably because a single Git repository contains the code for
+;; things released as multiple different JAR files on Clojars.
+
+;; Perhaps later when doing statistics on projects, it might be nice
+;; to use download counts for each Clojars.org artifact, but save that
+;; issue for a bi later.
+
+;; I want to download the git project exactly once when this happens.
+;; Pick whichever one happens to be first in my data with the same
+;; URL.
+
+;; The function add-canonical-artifact-ids helps to pick a 'canonical'
+;; artifact for each value of :source-url.
+
+(defn add-canonical-artifact-ids
+  "Given a sequence of maps describing Clojars artifacts, each map
+  containing:
+
+  + at least the keys :group-id and :artifact-id
+  + optionally a key :source-url with an associated string value
+
+  Return a sequence of maps with the same key/value pairs, except the
+  ones that have a :source-url key in the input will have the
+  key :canonical-artifact added to them.  The value of that key will
+  itself be a map containing keys :group-id and :artifact-id.
+
+  All maps in the sequence that have the same value associated with
+  the :source-url key will have the same value associated with
+  the :canonical-artifact key in the returned maps, and its value will
+  be the :group-id and :artifact-id of one arbitrarily chosen artifact
+  that had the same :source-url."
+  [art-maps]
+  (let [art-maps-by-source-url (group-by :source-url
+                                         (filter :source-url art-maps))]
+    (map (fn [art-map]
+           (if (contains? art-map :source-url)
+             (assoc art-map :canonical-artifact
+                    (select-keys (first (art-maps-by-source-url
+                                         (art-map :source-url)))
+                                 [:group-id :artifact-id]))
+             ;; else
+             art-map))
+         art-maps)))
+
+(defn canonical-artifact? [art-map]
+  (and (contains? art-map :canonical-artifact)
+       (let [can (:canonical-artifact art-map)]
+         (and (= (:group-id art-map) (:group-id can))
+              (= (:artifact-id art-map) (:artifact-id can))))))
 
 (defn read-clojars-feed [rdr]
   (let [sentinel-obj (Object.)]
@@ -135,6 +201,17 @@
         (if (identical? x sentinel-obj)
           (persistent! artifacts)
           (recur (conj! artifacts x)))))))
+
+(defn clean-artifact?
+  [art-map]
+  (and (contains? art-map :group-id)
+       (string? (art-map :group-id))
+       (not (str/includes? (art-map :group-id)
+                           group-id-artifact-id-separator))
+       (contains? art-map :artifact-id)
+       (string? (art-map :artifact-id))
+       (not (str/includes? (art-map :artifact-id)
+                           group-id-artifact-id-separator))))
 
 (defn pctg [n1 n2]
   (* 100.0 (/ (* 1.0 n1) n2)))
@@ -158,7 +235,10 @@
   maps `m` do, plus some extra keys as returned by (artifact-source
   m)."
   [clojars-feed-data]
-  (let [d (map #(merge % (artifact-source-loc %)) clojars-feed-data)
+  (let [n1 (count clojars-feed-data)
+        d1 (filter clean-artifact? clojars-feed-data)
+
+        d (map #(merge % (artifact-source-loc %)) d1)
         n (count d)
         loc-types (into (sorted-map)
                         (frequencies (map :artifact-loc-type d)))
@@ -170,13 +250,16 @@
         dup-group-artifact-ids (duplicates group-artifact-ids)
         ndups (reduce + (vals dup-group-artifact-ids))
 
-        msg1 (format "%d artifacts where
+        msg1 (format "%d artifacts, where
+%d remain after removing ones with 'unclean' data, out of which:
+
 %5d (%6.1f%%) appear to have usable URLs for accessing source code,
 %5d (%6.1f%%) have URLs that are less likely to be usable, and
 %5d (%6.1f%%) no URL likely to be usable at all was found.
 
 %5d (%6.1f%%) have unique (group-id:artifact-id) pair
 %5d (%6.1f%%) have (group-id:artifact-id) pair that is duplicate of another"
+                     n1
                      n
                      num-source-url (pctg num-source-url n)
                      num-source-url-maybe (pctg num-source-url-maybe n)
@@ -196,6 +279,43 @@ or :url key was found, or a :url key at the top level of the map:")
     {:data d,
      :string (str msg1 "\n\n" msg2 "\n" msg3)}))
 
+;; Note 1: On this StackOverflow question:
+
+;; https://serverfault.com/questions/544156/git-clone-fail-instead-of-prompting-for-credentials
+
+;; I learned of the technique of setting the environment variable
+;; GIT_TERMINAL_PROMPT to 0 to force 'git clone' to fail if it would
+;; normally try to ask you questions, such as entering account name
+;; and password, at the terminal.  I want getting any such git
+;; repository to fail rather than hanging, waiting for a response.
+
+(defn write-git-clone-bash-script [bash-fname base-dir-name art-maps]
+  (with-open [wrtr (io/writer bash-fname)]
+    (binding [*out* wrtr]
+      (println "#! /bin/bash")
+      (println)
+      ;; See Note 1
+      (println "export GIT_TERMINAL_PROMPT=0")
+      (println "mkdir -p " base-dir-name)
+      (doseq [art-map (filter :source-url art-maps)]
+        (let [ga-id (group-artifact-id art-map)]
+          (println)
+          (println "# " ga-id)
+          (if (canonical-artifact? art-map)
+            (do
+              (println "cd " base-dir-name)
+              (println "mkdir -p " ga-id)
+              (println "cd " ga-id)
+              (println "echo " (:source-url art-map) " > url.txt")
+              (println "git clone --recursive " (:source-url art-map))
+              (println "exit_status=$?")
+              (println "echo $exit_status > exit_status.txt"))
+            ;; else
+            (println "# canonical artifact has :group-id"
+                     (:group-id (:canonical-artifact art-map))
+                     " :artifact-id"
+                     (:artifact-id (:canonical-artifact art-map)))))))))
+
 
 (comment
 
@@ -214,9 +334,25 @@ or :url key was found, or a :url key at the top level of the map:")
            (->> r1
                 cloj/read-clojars-feed
                 cloj/summarize-clojars-feed-data)))
-(def as (:data tmp))
+;; as is short for 'artifacts'
+(def as (cloj/add-canonical-artifact-ids (:data tmp)))
 (print (:string tmp))
 (count as)
+
+(cloj/write-git-clone-bash-script "get-artifact-source2.sh"
+                                  "/home/andy/clj/haironfire/repos"
+                                  as)
+
+
+(count as)
+(def as-by-source-url (group-by :source-url (filter :source-url as)))
+(count as-by-source-url)
+
+(def as2 (cloj/add-canonical-artifact-ids as))
+(count as2)
+(def as3 (filter #(= (:source-url %) "http://www.github.com/zcaudate/vinyasa") as2))
+(count as3)
+(pprint (take 3 as3))
 
 (->> as (filter :source-url) (map :source-url) (take 100) pprint)
 
